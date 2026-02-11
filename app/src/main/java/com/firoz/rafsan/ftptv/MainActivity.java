95d096ecfdabcf36a6655f9403f5aa2491df5eba
Firoz <firozmahmud4009@gmail.com>
2026-02-11 20:40:44 +0600

    imdb done

--- app/src/main/java/com/firoz/rafsan/ftptv/MainActivity.java
+++ app/src/main/java/com/firoz/rafsan/ftptv/MainActivity.java
@@ -1,6 +1,8 @@
 package com.firoz.rafsan.ftptv;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Handler hand;
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        hand=new Handler();
+        tv=findViewById(R.id.tv);
+        (new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String data=imdb.searchIMDB("The Shawshank Redemption");
+                    hand.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText(data);
+                        }
+                    });
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/firoz/rafsan/ftptv/imdb.java
+++ app/src/main/java/com/firoz/rafsan/ftptv/imdb.java
@@ -1,4 +1,58 @@
 package com.firoz.rafsan.ftptv;
 
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+
 public class imdb {
+    private static String encodeData(String data) throws Exception{
+            return URLEncoder.encode(data, StandardCharsets.UTF_8.toString());
+        }
+
+    public static String searchIMDB(String title) throws Exception{
+        String data=sendGET("http://www.omdbapi.com/?apikey=64619439&t="+encodeData(title));
+        return data;
+    }
+    private static String sendGET(String urllink) throws Exception{
+        URL url = new URL(urllink);
+        HttpURLConnection connection = null;
+        StringBuilder response = new StringBuilder();
+
+        try {
+            // Open the connection
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            // Optional: Set a connection timeout (milliseconds)
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int responseCode = connection.getResponseCode();
+
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response from the input stream
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            } else {
+                throw new Exception("Failed to send get request in imdb");
+            }
+        } finally {
+            // Ensure the connection is always closed
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return response.toString();
+    }
 }
